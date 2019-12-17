dd9efa39fb2565294a5fd73d7fad1b1a06780b5f
dudusae <dudusae@gmail.com>
2019-12-17 12:24:41 +0900

    Fix Delete Error

--- src/task.js
+++ src/task.js
@@ -4,100 +4,113 @@ const taskForm = document.querySelector('.js-taskForm'),
   pendingList = document.querySelector('.js-pendingList'),
   finishedList = document.querySelector('.js-finishedList');
 
-const TODO_LS = 'TODO',
-  PENDING_LS = 'PENDING',
-  FINISHED_LS = 'FINISHED';
-
 let todos = [],
   pendings = [],
   finishedes = [];
 
-function getId() {
-  const id = Date.now()+Math.ceil(Math.random()*100);
-  return id;
-}
+const todo = {
+    ul: todoList,
+    key: 'TODO',
+    array: todos,
+  },
+  pending = {
+    ul: pendingList,
+    key: 'PENDING',
+    array: pendings,
+  },
+  finished = {
+    ul: finishedList,
+    key: 'FINISHED',
+    array: finishedes,
+  };
 
-function saveTasks(key, array) {
-  localStorage.setItem(key, JSON.stringify(array));
-}
-
-function deleteTask(e, ul, key, array) {
-    const btn = e.target;
-    const li = btn.parentNode;
-    const cleanTasks = array.filter(function(task) {
-      return task.id !== parseInt(li.id);
-    });
-    
-    ul.removeChild(li);
-    array = cleanTasks;
-    saveTasks(key, array);
+function getId(id) {
+  let newId = '';
+  if (id === undefined) {
+    newId = Date.now() + Math.ceil(Math.random() * 100);
+  } else {
+    return id;
   }
+  return newId;
+}
 
-function paintTodo(text) {
-  const newId = getId();
+function saveTasks(group) {
+  localStorage.setItem(group.key, JSON.stringify(group.array));
+}
+
+function deleteTask(e, group) {
+  const btn = e.target;
+  const li = btn.parentNode;
+  const ul = group.ul;
+  const cleanTasks = group.array.filter(function(task) {
+    return task.id !== parseInt(li.id);
+  });
+  ul.removeChild(li);
+  group.array = cleanTasks;
+  saveTasks(group);
+}
+
+function paintTodo(text, id) {
+  const newId = getId(id);
   const li = document.createElement('li');
   const delBtn = document.createElement('button');
   const startBtn = document.createElement('button');
+  const span = document.createElement('span');
+
   delBtn.innerHTML = '❌';
-  delBtn.addEventListener('click', (e)=> deleteTask(e, todoList, TODO_LS, todos));
+  delBtn.addEventListener('click', e => deleteTask(e, todo));
   startBtn.innerHTML = '🔥';
   startBtn.addEventListener('click', todoToPending);
-  const span = document.createElement('span');
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
   li.appendChild(startBtn);
   li.id = newId;
   todoList.appendChild(li);
+
   const todoObj = {
     text: text,
     id: newId,
   };
   todos.push(todoObj);
-  saveTasks(TODO_LS, todos);
+  saveTasks(todo);
 }
 
-function paintPending(text) {
-  const newId = getId();
+function paintPending(text, id, timeStamp) {
+  const newId = getId(id);
   const pendingLi = document.createElement('li');
   const pendingDelBtn = document.createElement('button');
   const finishBtn = document.createElement('button');
   const span = document.createElement('span');
   pendingDelBtn.innerHTML = '❌';
-  pendingDelBtn.addEventListener('click',  (e)=> deleteTask(e, pendingList, PENDING_LS, pendings));
+  pendingDelBtn.addEventListener('click', e => deleteTask(e, pending));
   finishBtn.innerHTML = '✅';
-  finishBtn.addEventListener('click', moveToFinish);
+  finishBtn.addEventListener('click', moveToFinished);
   span.innerText = text;
   pendingLi.appendChild(span);
   pendingLi.appendChild(pendingDelBtn);
   pendingLi.appendChild(finishBtn);
   pendingLi.id = newId;
   pendingList.appendChild(pendingLi);
-  
-  const taskObj = {
+
+  const pendingObj = {
     text: text,
-    timeStamp: Date.now(),
+    timeStamp: timeStamp,
     id: newId,
   };
-  pendings.push(taskObj);
-  saveTasks(PENDING_LS, pendings);
+  pendings.push(pendingObj);
+  saveTasks(pending);
 }
 
-function paintFinished(text, startTimeStamp) {
-  const newId = getId();
+function paintFinished(text, id, elapsedTime) {
+  const newId = getId(id);
   const fnLi = document.createElement('li');
   const fnDelBtn = document.createElement('button');
   const fnCancelBtn = document.createElement('button');
-    const fnSpan = document.createElement('span');
-    const endTimeStamp = Date.now();
-    const elapsedTimeObj = new Date(endTimeStamp- startTimeStamp-32400000);
-    const hours = elapsedTimeObj.getHours();
-    const minutes = elapsedTimeObj.getMinutes();
-    const seconds = elapsedTimeObj.getSeconds();
-    const elapsedTime = `[${hours}시간 ${minutes}분 ${seconds}초]`;
-    
+  const fnSpan = document.createElement('span');
+
   fnDelBtn.innerHTML = '❌';
-  fnDelBtn.addEventListener('click',  (e)=> deleteTask(e, finishedList, FINISHED_LS, finishedes));
+  fnDelBtn.addEventListener('click', e => deleteTask(e, finished));
   fnCancelBtn.innerHTML = '⛔';
   fnCancelBtn.addEventListener('click', finishedToPending);
 
@@ -108,37 +121,46 @@ function paintFinished(text, startTimeStamp) {
 
   fnLi.id = newId;
   finishedList.appendChild(fnLi);
-  const fnObj = {
+  const finishedObj = {
     text: text,
-    elapsedTime:  elapsedTime,
+    elapsedTime: elapsedTime,
     id: newId,
   };
-  finishedes.push(fnObj);
-  saveTasks(FINISHED_LS, finishedes);
+  finishedes.push(finishedObj);
+  saveTasks(finished);
 }
 
-function moveToFinish(e) {
+function moveToFinished(e) {
   const moveBtn = e.target;
   const moveLi = moveBtn.parentNode;
   const moveTasks = pendings.filter(function(task) {
     return task.id === parseInt(moveLi.id);
   });
-  const moveText = moveTasks[0].text;
+  const text = moveTasks[0].text;
+  const id = moveTasks[0].id;
   const startTimeStamp = moveTasks[0].timeStamp;
-  paintFinished(moveText, startTimeStamp);
-  deleteTask(e, pendingList, PENDING_LS, pendings);
+  const endTimeStamp = Date.now();
+  const elapsedTimeObj = new Date(endTimeStamp - startTimeStamp - 32400000);
+  const hours = elapsedTimeObj.getHours();
+  const minutes = elapsedTimeObj.getMinutes();
+  const seconds = elapsedTimeObj.getSeconds();
+  const elapsedTime = `[${hours}시간 ${minutes}분 ${seconds}초]`;
+  paintFinished(text, id, elapsedTime);
+  deleteTask(e, pending);
 }
 
 function todoToPending(e) {
-    const moveTdBtn = e.target;
-    const moveTdLi = moveTdBtn.parentNode;
-    const moveTdTasks = todos.filter(function(task) {
-      return task.id === parseInt(moveTdLi.id);
-    });
-    const moveTdText = moveTdTasks[0].text;
-    paintPending(moveTdText);
-    deleteTask(e, todoList, TODO_LS, todos);
-  }
+  const moveBtn = e.target;
+  const moveLi = moveBtn.parentNode;
+  const moveTasks = todos.filter(function(task) {
+    return task.id === parseInt(moveLi.id);
+  });
+  const text = moveTasks[0].text;
+  const id = moveTasks[0].id;
+  const timeStamp = Date.now(); 
+  paintPending(text, id, timeStamp);
+  deleteTask(e, todo);
+}
 
 function finishedToPending(e) {
   const moveFnBtn = e.target;
@@ -147,8 +169,9 @@ function finishedToPending(e) {
     return task.id === parseInt(moveFnLi.id);
   });
   const moveFnText = movefinishedes[0].text;
-  paintPending(moveFnText);
-  deleteTask(e, finishedList, FINISHED_LS, finishedes);
+  const moveFnId = movefinishedes[0].id;
+  paintPending(moveFnText, moveFnId);
+  deleteTask(e, finished);
 }
 
 function handleSubmit(e) {
@@ -159,28 +182,29 @@ function handleSubmit(e) {
 }
 
 function loadTasks() {
-  const loadedTodo = localStorage.getItem(TODO_LS);
-  const loadedPending = localStorage.getItem(PENDING_LS);
-  const loadedFinished = localStorage.getItem(FINISHED_LS);
+  const loadedTodo = localStorage.getItem(todo.key);
+  const loadedPending = localStorage.getItem(pending.key);
+  const loadedFinished = localStorage.getItem(finished.key);
+  if (loadedTodo !== null) {
+    const parsedTodo = JSON.parse(loadedTodo);
+    parsedTodo.forEach(function(todo) {
+      paintTodo(todo.text, todo.id);
+    });
+  }
   if (loadedPending !== null) {
     const parsedPending = JSON.parse(loadedPending);
-    parsedPending.forEach(function(task) {
-      paintPending(task.text);
+    parsedPending.forEach(function(pending) {
+      paintPending(pending.text, pending.id, pending.timeStamp);
     });
   }
   if (loadedFinished !== null) {
     const parsedFinished = JSON.parse(loadedFinished);
     parsedFinished.forEach(function(finished) {
-      paintFinished(finished.text);
-    });
-  }
-  if (loadedTodo !== null) {
-    const parsedTodo = JSON.parse(loadedTodo);
-    parsedTodo.forEach(function(todo) {
-      paintTodo(todo.text);
+      paintFinished(finished.text, finished.id, finished.elapsedTime);
     });
   }
 }
+
 function init() {
   loadTasks();
   taskForm.addEventListener('submit', handleSubmit);
