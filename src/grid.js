3193cae395160b38c3b3d28b7c16d1eab1e32574
dudusae <dudusae@gmail.com>
2019-12-24 15:08:47 +0900

    Update Grid - editable Text cell

--- src/grid.js
+++ src/grid.js
@@ -1,8 +1,8 @@
 const Grid = tui.Grid;
+const deleteBtn = document.querySelector('.js-deleteBtn');
 
 let saves = [];
 const saved = { key: 'SAVED', array: saves };
-
 const loadedSaved = localStorage.getItem(saved.key);
 const parseSaved = JSON.parse(loadedSaved);
 
@@ -12,7 +12,7 @@ function loadSaved() {
     const parsedSaved = JSON.parse(loadedSaved);
 
     parsedSaved.forEach(function(save) {
-      saves.push(save);
+      saved.array.push(save);
     });
   }
 }
@@ -21,18 +21,48 @@ function saveTasks(group) {
   localStorage.setItem(group.key, JSON.stringify(group.array));
 }
 
-function updateSaved(ev) {
+function deleteTask(checkedId) {
+  saved.array.length = 0;
+  loadSaved();
+  const cleanTasks = saved.array.filter(function(task) {
+    return task.id !== parseInt(checkedId);
+  });
+  saved.array = cleanTasks;
+  // saves와 saved.array가 왜 다른걸까
+  saveTasks(saved);
+}
+
+function deleteRows() {
+  const checkedRows = grid.getCheckedRows();
+  const checkedTotal = checkedRows.length;
+  const confirmMessage = `삭제하면 복구가 안돼요. ${checkedTotal}개의 업무내역을 삭제하실래요?`;
+
+  if (confirm(confirmMessage)) {
+    checkedRows.forEach(row => deleteTask(row.id));
+    location.reload();
+  }
+}
+
+function updateProject(ev) {
   const targetRow = ev.rowKey;
   const targetId = grid.getValue(targetRow, 'id', true);
-  const targetIdx = saves.findIndex(save => save.id === targetId);
-  saves[targetIdx].project = ev.value;
+  const targetIdx = saved.array.findIndex(save => save.id === targetId);
+  saved.array[targetIdx].project = ev.value;
+  saveTasks(saved);
+}
+
+function updateText(ev) {
+  const targetRow = ev.rowKey;
+  const targetId = grid.getValue(targetRow, 'id', true);
+  const targetIdx = saved.array.findIndex(save => save.id === targetId);
+  saved.array[targetIdx].text = ev.value;
   saveTasks(saved);
 }
 
 const options = {
   el: document.getElementById('grid'),
   width: 700,
-  rowHeaders : [{type: 'checkbox'}],
+  rowHeaders: [{ type: 'checkbox' }],
   columns: [
     { header: '아이디', name: 'id', align: 'center' },
     {
@@ -40,7 +70,7 @@ const options = {
       name: 'project',
       editor: 'text',
       onAfterChange(ev) {
-        updateSaved(ev);
+        updateProject(ev);
       },
       align: 'center',
     },
@@ -48,6 +78,9 @@ const options = {
       header: '업무내용',
       name: 'text',
       editor: 'text',
+      onAfterChange(ev) {
+        updateText(ev);
+      },
       align: 'center',
     },
     {
@@ -67,11 +100,19 @@ const options = {
 const grid = new Grid(options);
 
 Grid.applyTheme('clean', {
-  grid: {
-    border: '#aaa',
-    text: '#333',
-  },
   cell: {
+    normal:{
+      text: '#222',
+      border: '#c9c9c9',
+      background:'#fff',
+      showVerticalBorder: false,
+    },
+    header:{
+      text: '#767676',
+      border: '#c9c9c9',
+      background:'#fff',
+      showVerticalBorder: false,
+    },
     disabled: {
       text: '#999',
       align: 'center',
@@ -79,13 +120,15 @@ Grid.applyTheme('clean', {
   },
   row: {
     hover: {
-      background: '#f3ecfd',
+      text: '#00c85e'
     },
   },
 });
 
 function init() {
   loadSaved();
+  deleteBtn.addEventListener('click', deleteRows);
+  grid.hideColumn('id');
 }
 
 init();
